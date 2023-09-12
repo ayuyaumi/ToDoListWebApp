7d6b348dfd61f0191753ab8dc0638875140068bc
ayuyaumi <sitir32@gmail.com>
2023-09-12 15:55:45 +0800

    Add project files.

---
+++ ToDoListWebApp/Models/Status.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListWebApp.Models
+{
+    public enum Status
+    {
+        Created = 1,
+        InProgress,
+        Completed
+    }
+}
---
+++ ToDoListWebApp/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListWebApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public Status Status { get; set; } = Status.Created;
+    }
+}
